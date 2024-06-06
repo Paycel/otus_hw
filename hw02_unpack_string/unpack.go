d76789797ff9795d54a514e804f4d00e5202a34f
Nikita <huskyray@mail.ru>
2024-06-06 23:08:36 +0300

    add more unicode tests

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,8 +12,9 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(input string) (string, error) {
 	var builder strings.Builder
 	var last string
+	size := utf8.RuneCountInString(input)
 
-	for i, r := range input {
+	for i, r := range []rune(input) { // not `range input` for correct 'i' rune counter
 		char := string(r)
 
 		d, errCD := strconv.Atoi(char) // is current digit
@@ -27,7 +28,7 @@ func Unpack(input string) (string, error) {
 			if len(last) > 0 && errLD != nil {
 				builder.WriteString(last)
 			}
-			if i == utf8.RuneCountInString(input)-1 {
+			if i == size-1 {
 				builder.WriteRune(r)
 			}
 		} else { // digit
